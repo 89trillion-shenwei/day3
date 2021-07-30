e43e05bde610d1baa8a9d119d299d1c9a0ced04d
shenwei <shenwei@89trillion.com>
2021-07-30 18:20:50 +0800

    修改同步问题

--- internal/service/dealData.go
+++ internal/service/dealData.go
@@ -157,12 +157,14 @@ func (User *User) StrUpdate(key string) (string, error) {
 	c2 := RedisPool1.Get()
 	defer c1.Close()
 	defer c2.Close()
-	/*//上锁
-	releaseLock()*/
+	//上锁
+	releaseLock()
 	//查询数据
 	res1, err1 := redis.String(c1.Do("Get", key))
 	res2, err2 := redis.String(c2.Do("Get", key))
 	if err1 != nil || err2 != nil {
+		//解锁
+		getLock()
 		return "查询礼品时出错", internal.InternalServiceError(err1.Error() + err2.Error())
 	} else {
 		var byts1, byts2 []byte
@@ -176,6 +178,8 @@ func (User *User) StrUpdate(key string) (string, error) {
 		}
 		//如果礼品可领取次数为0，退出
 		if mess.AvailableTimes == "0" {
+			//解锁
+			getLock()
 			return "", internal.NoGiftError("礼品已领完")
 		}
 		//指定用户一次性消耗
@@ -199,6 +203,8 @@ func (User *User) StrUpdate(key string) (string, error) {
 				//提交更改后的数据
 				_, err := c2.Do("SET", key, struct2json(mess))
 				if err != nil {
+					//解锁
+					getLock()
 					return "", internal.InternalServiceError(err.Error())
 				} else {
 					fmt.Println("set ok.")
@@ -207,9 +213,13 @@ func (User *User) StrUpdate(key string) (string, error) {
 				_, err1 := redis.Int64(c2.Do("INCR", "key"))
 				if err1 != nil {
 					log.Println("INCR failed:", err)
+					//解锁
+					getLock()
 					return "", internal.InternalServiceError(err1.Error())
 				}
 			} else {
+				//解锁
+				getLock()
 				return "", internal.NoCanGetUserError("非指定用户")
 			}
 		} else if message.CodeType == "2" { //不指定用户,限制次数
@@ -220,6 +230,8 @@ func (User *User) StrUpdate(key string) (string, error) {
 			getList.GetTime = time.Now().Format("2006-01-02 15:04:05")
 			//判断该用户是否已经使用过该礼品码
 			if findUser(mess.GetList, User.UserName) {
+				//解锁
+				getLock()
 				return "", internal.UserHasEeceivedError("你已使用过该礼品码")
 			}
 			mess.GetList = append(mess.GetList, *getList)
@@ -234,6 +246,8 @@ func (User *User) StrUpdate(key string) (string, error) {
 			//提交更改后的数据
 			_, err := c2.Do("SET", key, struct2json(mess))
 			if err != nil {
+				//解锁
+				getLock()
 				return "", internal.InternalServiceError(err.Error())
 			} else {
 				fmt.Println("set ok.")
@@ -242,6 +256,8 @@ func (User *User) StrUpdate(key string) (string, error) {
 			_, err1 := redis.Int64(c2.Do("INCR", "key"))
 			if err1 != nil {
 				log.Println("INCR failed:", err)
+				//解锁
+				getLock()
 				return "", internal.InternalServiceError(err1.Error())
 			}
 		} else { //不指定用户,不限制次数
@@ -252,6 +268,8 @@ func (User *User) StrUpdate(key string) (string, error) {
 			getList.GetTime = time.Now().Format("2006-01-02 15:04:05")
 			//判断该用户是否已经使用过该礼品码
 			if findUser(mess.GetList, User.UserName) {
+				//解锁
+				getLock()
 				return "你已使用过该礼品码", internal.UserHasEeceivedError("你已使用过该礼品码")
 			}
 			mess.GetList = append(mess.GetList, *getList)
@@ -263,6 +281,8 @@ func (User *User) StrUpdate(key string) (string, error) {
 			//提交更改后的数据
 			_, err := c2.Do("SET", key, struct2json(mess))
 			if err != nil {
+				//解锁
+				getLock()
 				return "", internal.InternalServiceError(err.Error())
 			} else {
 				fmt.Println("set ok.")
@@ -271,11 +291,13 @@ func (User *User) StrUpdate(key string) (string, error) {
 			_, err1 := redis.Int64(c2.Do("INCR", "key"))
 			if err1 != nil {
 				log.Println("INCR failed:", err)
+				//解锁
+				getLock()
 				return "", internal.InternalServiceError(err1.Error())
 			}
 		}
-		/*//解锁
-		getLock()*/
+		//解锁
+		getLock()
 		//返回礼品内容
 		return string("您将获得的礼品有：" + string(struct2json(message.List))), nil
 	}
